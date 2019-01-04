b5529a4eeab56878bb23733b4940525118a90241
BrendanJHarris <brendanjohnharris@gmail.com>
2019-01-04 21:16:19 +1100

    Added script to add all subfolders to matlab search path

---
+++ add_all_subfolders.m
@@ -0,0 +1,7 @@
+home = pwd;
+paths = regexp(genpath(home),'[^;]*','match');
+paths = strsplit(paths{1}, ':');
+paths = paths(~contains(paths, [filesep, '.'])); % Leave out .git and other unwanted files
+for i = 1:length(paths)
+    addpath(paths{i})
+end
