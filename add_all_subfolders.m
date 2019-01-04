f648b9a8cd9a4946dc743033e7b5c64dc0de6cd3
BrendanJHarris <brendanjohnharris@gmail.com>
2019-01-05 03:23:16 +1100

    Changed to a function

--- add_all_subfolders.m
+++ add_all_subfolders.m
@@ -1,7 +1,9 @@
-home = pwd;
-paths = regexp(genpath(home),'[^;]*','match');
-paths = strsplit(paths{1}, ':');
-paths = paths(~contains(paths, [filesep, '.'])); % Leave out .git and other unwanted files
-for i = 1:length(paths)
-    addpath(paths{i})
+function add_all_subfolders
+    home = pwd;
+    paths = regexp(genpath(home),'[^;]*','match');
+    paths = strsplit(paths{1}, ':');
+    paths = paths(~contains(paths, [filesep, '.'])); % Leave out .git and other unwanted files
+    for i = 1:length(paths)
+        addpath(paths{i})
+    end
 end
