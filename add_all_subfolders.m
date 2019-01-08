c7f941eedac628f4300ebca58df62d72e236d88f
BrendanJHarris <brendanjohnharris@gmail.com>
2019-01-08 18:54:04 +1100

    Fixed for for Windows

--- add_all_subfolders.m
+++ add_all_subfolders.m
@@ -1,7 +1,7 @@
 function add_all_subfolders
     home = pwd;
-    paths = regexp(genpath(home),'[^;]*','match');
-    paths = strsplit(paths{1}, ':');
+    paths = regexp(genpath(home),['[^', pathsep, ']*'],'match');
+    %paths = strsplit(paths{1}, ':');
     paths = paths(~contains(paths, [filesep, '.'])); % Leave out .git and other unwanted files
     for i = 1:length(paths)
         addpath(paths{i})
