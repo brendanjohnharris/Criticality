aefafac7aaea0b19243f6a2e6018251edf7dee06
BrendanJHarris <brendanjohnharris@gmail.com>
2019-01-23 21:15:21 +1100

    Added function to sort time_series_data

---
+++ Feature_Analysis/sort_data.m
@@ -0,0 +1,14 @@
+function dataout = sort_data(datain)
+    % This function shouldn't be necessary
+    dataout = struct(datain);
+    for ind = 1:size(datain, 1)
+        data = datain(ind, :);
+        [~, idxs] = sort(data.Operations.ID);
+        data.Operations = data.Operations(idxs, :);
+        data.TS_DataMat = data.TS_DataMat(:, idxs); % TS_Datamat should be in the same order as Operations
+        [~, idxcor] = sort(data.Correlation(:, 2)); % Correlation may not be in the same order as Operations, but is labelled with feature IDs
+        data.Correlation = data.Correlation(idxcor, :);
+        dataout(ind, :) = data;
+    end
+end
+
