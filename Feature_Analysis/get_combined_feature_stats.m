1e1a43e4bd4471fe05e39091fe6c8565de31d2b0
BrendanJHarris <brendanjohnharris@gmail.com>
2019-02-23 14:13:55 +1100

    Fixed errors due to overlapping column names, added more stats

--- Feature_Analysis/get_combined_feature_stats.m
+++ Feature_Analysis/get_combined_feature_stats.m
@@ -3,57 +3,94 @@ function tbl = get_combined_feature_stats(data, single_stats, combined_stats, di
     for i = 1:length(table_container)
         table_container{i} = get_feature_stats(data(i, :), single_stats, directions, []);
     end
-    tbl = merge_tables(table_container, arrayfun(@(x) ['data', num2str(x), '_'], 1:size(data, 1), 'UniformOutput', false));
+    original_tbl = merge_tables(table_container, arrayfun(@(x) ['data', num2str(x), '_'], 1:size(data, 1), 'UniformOutput', false));
+    tbl = original_tbl;
     merged_table_size = size(tbl, 2);
     for the_stat = combined_stats
         try
             switch the_stat{1}
                 case 'Correlation_Mean'
-                    what_columns = contains(tbl.Properties.VariableNames, 'Correlation')&~contains(tbl.Properties.VariableNames, 'Absolute_Correlation');
-                    the_stat_values = mean(tbl(:, what_columns));
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Correlation')&~contains(original_tbl.Properties.VariableNames, 'Absolute_Correlation');
+                    the_stat_values = mean(original_tbl(:, what_columns));
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stat 'Correlation'", the_stat{1})
+                    end
+                    
                 case 'Absolute_Correlation_Mean'
-                    what_columns = contains(tbl.Properties.VariableNames, 'Absolute_Correlation');
-                    the_stat_values = mean(tbl{:, what_columns}, 2);
-
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Absolute_Correlation');
+                    the_stat_values = mean(original_tbl{:, what_columns}, 2);
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stat 'Absolute_Correlation'", the_stat{1})
+                    end
+                    
                 case 'Peak_Shift_Mean'
-                    what_columns = contains(tbl.Properties.VariableNames, 'Peak_Shift');
-                    the_stat_values = mean(tbl{:, what_columns}, 2);
-                    
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Peak_Shift');
+                    the_stat_values = mean(original_tbl{:, what_columns}, 2);
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stat 'Peak_Shift'", the_stat{1})
+                    end
+                                        
                 case 'Feature_Value_Gradient_SD'
-                    what_columns = contains(tbl.Properties.VariableNames, 'Feature_Value_Gradient');
-                    the_stat_values = std(tbl{:, what_columns},[], 2);
-                    
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_Gradient');
+                    the_stat_values = std(original_tbl{:, what_columns},[], 2);
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stat 'Feature_Value_Gradient'", the_stat{1})
+                    end
+                                        
                 case 'Feature_Value_Intercept_SD'
-                    what_columns = contains(tbl.Properties.VariableNames, 'Feature_Value_Intercept');
-                    the_stat_values = std(tbl{:, what_columns},[], 2);
-                    
-                case 'Relative_Feature_Value_Intercept_SD'
-                    what_columns = contains(tbl.Properties.VariableNames, 'Feature_Value_Intercept');
-                    the_stat_values = std(tbl{:, what_columns},[], 2);
-                    what_columns = contains(tbl.Properties.VariableNames, 'Feature_Value_RMSE');
-                    mean_RMSE = mean(tbl{:, what_columns}, 2);
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_Intercept');
+                    the_stat_values = std(original_tbl{:, what_columns},[], 2);
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stat 'Feature_Value_Intercept'", the_stat{1})
+                    end
+                
+                case {'RMSE_Scaled_Feature_Value_Gradient', 'RMSE_Scaled_Feature_Value_Intercept'}
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_Intercept');
+                    the_stat_values = std(original_tbl{:, what_columns},[], 2);
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_RMSE');
+                    mean_RMSE = mean(original_tbl{:, what_columns}, 2);
                     the_stat_values = the_stat_values./mean_RMSE;
-                    
+                case 'Relative_Feature_Value_Intercept_SD'
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_Intercept');
+                    the_stat_values = std(original_tbl{:, what_columns},[], 2);
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_RMSE');
+                    mean_RMSE = mean(original_tbl{:, what_columns}, 2);
+                    the_stat_values = the_stat_values./mean_RMSE;
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stats 'Feature_Value_Intercept' and 'Feature_Value_RMSE'", the_stat{1})
+                    end
+                                        
                 case 'Feature_Value_Gradient_RMSE'
-                    what_columns = contains(tbl.Properties.VariableNames, 'Feature_Value_Intercept');
-                    y = num2cell(tbl{:, what_columns}, 2);
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_Intercept');
+                    y = num2cell(original_tbl{:, what_columns}, 2);
                     x = arrayfun(@(x) x.Inputs.eta, data)';
                     the_stat_values = sqrt(cell2mat(cellfun(@(y) immse(x, y), y, 'uniformoutput', 0)));
-                
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stat 'Feature_Value_Intercept'", the_stat{1})
+                    end
+                                    
                 case 'Aggregated_Absolute_Correlation'
                     idxs = (data(1).Inputs.cp_range >= data(1).Correlation_Range(1) & data(1).Inputs.cp_range <= data(1).Correlation_Range(2)); % Assumes all rows of data have the same cp_range and Correlation_Range
                     y = cell2mat(arrayfun(@(x) data(x, :).TS_DataMat(idxs, :), 1:size(data, 1), 'UniformOutput', 0)');
                     x = cell2mat(arrayfun(@(x) data(x, :).Inputs.cp_range(idxs), 1:size(data, 1), 'UniformOutput', 0))';
                     the_stat_values = abs(corr(y, x, 'Type', 'Pearson'));
-                
+                                    
                 case 'Feature_Value_Gradient_Absolute_Correlation'
-                    what_columns = contains(tbl.Properties.VariableNames, 'Feature_Value_Gradient');
-                    the_stat_values = abs(corr(arrayfun(@(x) x.Inputs.eta, data), tbl{:, what_columns}')');
-                
+                    what_columns = regexp(original_tbl.Properties.VariableNames, '.*\d+_Feature_Value_Gradient', 'match');
+                    what_columns = cellfun(@(x) ~isempty(x), what_columns);
+                    the_stat_values = abs(corr(arrayfun(@(x) x.Inputs.eta, data), original_tbl{:, what_columns}')');
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stat 'Feature_Value_Gradient'", the_stat{1})
+                    end
+                                    
                 case 'Feature_Value_Intercept_Absolute_Correlation'
-                    what_columns = contains(tbl.Properties.VariableNames, 'Feature_Value_Intercept');
-                    the_stat_values = abs(corr(arrayfun(@(x) x.Inputs.eta, data), tbl{:, what_columns}')');                    
-                
+                    what_columns = regexp(original_tbl.Properties.VariableNames, '.*\d+_Feature_Value_Intercept');
+                    what_columns = cellfun(@(x) ~isempty(x), what_columns);
+                    the_stat_values = abs(corr(arrayfun(@(x) x.Inputs.eta, data), original_tbl{:, what_columns}')');                    
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stat 'Feature_Value_Intercept'", the_stat{1})
+                    end
+                                    
                 case {'Normalised_Feature_Value_Gradient_SD', 'Normalised_Feature_Value_Intercept_SD'} % Should eventually integrate into find_correlation
                     % Gives the gradient of values up until 0
                     idxs = (data(1).Inputs.cp_range >= data(1).Correlation_Range(1) & data(1).Inputs.cp_range <= data(1).Correlation_Range(2))'; % Assume cp_range and correlation range are the same for all rows
@@ -72,15 +109,96 @@ function tbl = get_combined_feature_stats(data, single_stats, combined_stats, di
                         the_stat_values = std(m, 1)';
                     elseif strcmp(the_stat{1}, 'Normalised_Feature_Value_Intercept_SD')
                         the_stat_values = std(b, 1)';
-                    end                      
-
-                otherwise
+                    end
+                                    
+                case 'Average_RMSE_Constant_Gradient'
+                    % Get the linear fit of the intercepts
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_Intercept');
+                    b = tbl{:, what_columns}';
+                    noise = arrayfun(@(x) x.Inputs.eta, data);
+                    m_b = corr(b, noise, 'Type', 'Pearson').*std(b, [], 1)'./std(noise);
+                    b_b = mean(b, 1)' - m_b.*mean(noise);
+                    % m_b and b_b have features down their rows
+                    % Get the average gradient
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_Gradient');
+                    m = mean(tbl{:, what_columns}, 2); % Maybe use median instead of mean, for the features that spike dramatically near 0 noise?
+                    % Calculate intercept estimates from intercept trend
+                    b = m_b.*noise' + b_b;
+                    RMSE = zeros(size(b));
+                    % Only use control parameters that are in the
+                    % correlation range: Assumes the correlation range is
+                    % the same for all eta (might have to fix for
+                    % subcritical)
+                    idxs = (data(1).Inputs.cp_range >= data(1).Correlation_Range(1) & data(1).Inputs.cp_range <= data(1).Correlation_Range(2))';
+                    % Calculate RMSE (of predicting CONTROL PARAMETER from
+                    % FEATURE VALUES by PREDICTING THE FIT INTERCEPT and
+                    % ASSUMING THE FIT GRADIENT IS CONSTANT
+                    for n = 1:size(b, 2)
+                        RMSE(:, n) = sqrt(sum((data(n).Inputs.cp_range(idxs) - ((data(n).TS_DataMat(idxs, :)' - b(:, n))./m)).^2, 2)./length(data(n).Inputs.cp_range(idxs)));
+                    end
+                    % Average these RMSE's for each feature (over the noise)
+                    the_stat_values = mean(RMSE, 2);
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stats 'Feature_Value_Intercept' and 'Feature_Value_Gradient'", the_stat{1})
+                    end
+                                        
+                case 'Average_RMSE_Constant_Intercept'
+                     % Get the linear fit of the gradients
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_Gradient');
+                    m = tbl{:, what_columns}';
+                    noise = arrayfun(@(x) x.Inputs.eta, data);
+                    m_m = corr(m, noise, 'Type', 'Pearson').*std(m, [], 1)'./std(noise);
+                    b_m = mean(m, 1)' - m_m.*mean(noise);
+                    % m_m (gradients of gradient) and b_m (intercepts of gradient) have features down their rows
+                    % Get the average intercept
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Feature_Value_Intercept');
+                    b = mean(tbl{:, what_columns}, 2); % Maybe use median instead of mean, for the features that spike dramatically near 0 noise?
+                    % Calculate gradient estimates from intercept trend
+                    m = m_m.*noise' + b_m;
+                    RMSE = zeros(size(m));
+                    % Only use control parameters that are in the
+                    % correlation range: Assumes the correlation range is
+                    % the same for all eta (might have to fix for
+                    % subcritical)
+                    idxs = (data(1).Inputs.cp_range >= data(1).Correlation_Range(1) & data(1).Inputs.cp_range <= data(1).Correlation_Range(2))';
+                    % Calculate RMSE (of predicting CONTROL PARAMETER from
+                    % FEATURE VALUES by PREDICTING THE FIT GRADIENT and
+                    % ASSUMING THE FIT INTERCEPT IS CONSTANT
+                    for n = 1:size(m, 2)
+                        RMSE(:, n) = sqrt(sum((data(n).Inputs.cp_range(idxs) - ((data(n).TS_DataMat(idxs, :)' - b)./m(:, n))).^2, 2)./length(data(n).Inputs.cp_range(idxs)));
+                    end
+                    % Average these RMSE's for each feature (over the noise)
+                    the_stat_values = mean(RMSE, 2);
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stats 'Feature_Value_Intercept' and 'Feature_Value_Gradient'", the_stat{1})
+                    end
+                
+                case 'Mean_Control_Parameter_RMSE'
+                    what_columns = contains(original_tbl.Properties.VariableNames, 'Control_Parameter_RMSE');
+                    the_stat_values = mean(tbl{:, what_columns}, 2);
+                    if sum(what_columns) == 0
+                        error("'%s' requires the single_stat'Control_Parameter_RMSE'", the_stat{1})
+                    end
+                 
+                case 'Multiple_Linear_Regression_Coefficients'
+                    the_stat_values = get_linear_model(data);
+                
+                case 'Multiple_Linear_Regression_RMSE'
+                    [~, the_stat_values] = get_linear_model(data);
+                
+                case 'Multiple_Linear_Regression_Coefficients_Zero_Noise_Partial'
+                    the_stat_values = get_linear_model(data, [], 0);
+                
+                case 'Multiple_Linear_Regression_RMSE_Zero_Noise_Partial'
+                    [~, the_stat_values] = get_linear_model(data, [], 0);
+                    
+                    otherwisedit
                     error([the_stat{1}, ' is not a supported statistic'])
             end
         catch ME
             switch ME.identifier
                 case 'MATLAB:sum:wrongInput'
-                    error('The single_stats given do not include a valid statistic for the combined_stat %s', the_stat{1})
+                    error("The single_stats given do not include a valid statistic for the combined_stat '%s'", the_stat{1})
                 otherwise
                     rethrow(ME)
             end
