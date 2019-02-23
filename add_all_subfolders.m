3ae18ddc92a587a496509574783259f17b5043c9
BrendanJHarris <brendanjohnharris@gmail.com>
2019-02-23 14:11:25 +1100

    Changed variable name, in case of overloading

--- add_all_subfolders.m
+++ add_all_subfolders.m
@@ -1,8 +1,8 @@
 function add_all_subfolders
 %ADD_ALL_SUBFOLDERS Adds all subdirectories of the current directory to the
 %search path, recursively
-    home = pwd;
-    paths = regexp(genpath(home),['[^', pathsep, ']*'],'match');
+    homedir = pwd;
+    paths = regexp(genpath(homedir),['[^', pathsep, ']*'],'match');
     paths = paths(~contains(paths, [filesep, '.'])); % Leave out .git and other unwanted files
     for i = 1:length(paths)
         addpath(paths{i})
